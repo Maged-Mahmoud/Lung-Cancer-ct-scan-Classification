982b8be096499496801711d8e703b5bd608d6690
Maged Mahmoud <65985410+Maged-Mahmoud@users.noreply.github.com>
2022-07-01 15:34:15 +0400

    Update deploy-app.py

--- deploy-app.py
+++ deploy-app.py
@@ -56,8 +56,7 @@ uploaded_file = st.sidebar.file_uploader(" ",type=['png', 'jpg', 'jpeg'] )
 
 if uploaded_file is not None:
     
-    image = uploaded_file
-    image = Image.resize((460,460))
+    image = Image.open(uploaded_file).resize((460,460))
     show.image(image, 'Uploaded Image', use_column_width=True)
     # We preprocess the image to fit in algorithm.
     
